3c3c47876f6ca393a02ad045b99c6554e76b6f8d
sbl <stephen.lumenta@gmail.com>
2016-07-17 19:19:47 +0200

    homebrew installs

---
+++ brew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# install homebrew things we like
+
+brew install zsh
+brew install git
+brew install node
+brew install go
+brew install neovim/neovim/neovim
+brew install trash
+brew install pyenv
+brew install pyenv-virtualenv

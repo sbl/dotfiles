e04bda74f8880f52cb0574842fa16590ccbf6e44
sbl <stephen.lumenta@gmail.com>
2016-11-06 02:56:42 +0100

    update brew file

--- brew.sh
+++ brew.sh
@@ -8,5 +8,7 @@ brew install the_silver_searcher
 brew install git
 brew install node
 brew install go
+brew install python
+brew install python3
 brew install neovim/neovim/neovim
 brew install trash

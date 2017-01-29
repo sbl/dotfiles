73f7a3fe4dc58c66c2fda2719df463d49c35df1e
sbl <stephen.lumenta@gmail.com>
2017-01-29 14:11:02 +0100

    ls alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,6 +25,7 @@ bind \ee accept-autosuggestion # ctrl-e completes first suggestion
 # aliases
 
 alias l='ls -lFh'
+alias ll='ls -alF'
 alias vi=nvim
 alias vim=nvim
 alias gll='git log --graph --pretty=format:"%C(bold red)%h%C(reset)%C(yellow)%d%C(reset) %C(red)(%cr)%C(reset) %s — %C(blue)%ae%C(reset)" --abbrev-commit'

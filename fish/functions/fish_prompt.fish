e69a99ea401e88cd1ca6e4cb3b67c804f57c0a0a
sbl <stephen.lumenta@gmail.com>
2017-05-14 12:19:06 +0200

    clean

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,3 @@
-# Fish git prompt
-#set __fish_git_prompt_showdirtystate 'yes'
-#set __fish_git_prompt_char_dirtystate '⚡'
-
-#set __fish_git_prompt_showstagedstate 'yes'
-#set __fish_git_prompt_char_stagedstate '*'
-
-#set __fish_git_prompt_showupstream 'auto'
-#set __fish_git_prompt_char_upstream_ahead '+'
-#set __fish_git_prompt_char_upstream_behind '-'
-
-#set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_show_informative_status 'yes'
 
 function _pwd_with_tilde

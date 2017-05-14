fea50f9695f3f37111cd994b300dc8f192966060
sbl <stephen.lumenta@gmail.com>
2017-05-14 12:18:20 +0200

    info

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,16 @@
 # Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_char_dirtystate '⚡'
+#set __fish_git_prompt_showdirtystate 'yes'
+#set __fish_git_prompt_char_dirtystate '⚡'
 
-set __fish_git_prompt_showstagedstate 'yes'
-set __fish_git_prompt_char_stagedstate '*'
+#set __fish_git_prompt_showstagedstate 'yes'
+#set __fish_git_prompt_char_stagedstate '*'
 
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+#set __fish_git_prompt_showupstream 'auto'
+#set __fish_git_prompt_char_upstream_ahead '+'
+#set __fish_git_prompt_char_upstream_behind '-'
 
-set __fish_git_prompt_showuntrackedfiles 'yes'
-
+#set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_show_informative_status 'yes'
 
 function _pwd_with_tilde
   echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|'

d6f1d180c0af3be4f3af726a1faa56207642cb3c
sbl <stephen.lumenta@gmail.com>
2017-05-26 17:16:28 +0200

    better bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-mkdir -p $HOME/.config/{fish,nvim}
+mkdir -p $HOME/.config/fish
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/bin
 
-stow -vv -t $HOME/.config/fish fish
-stow -vv -t $HOME/.config/nvim nvim
-stow -vv -t $HOME/bin bin
-stow -vv -t $HOME misc
+stow -v2 --ignore 'fishd.*' -t $HOME/.config/fish fish
+stow -v2 -t $HOME/.config/nvim nvim
+stow -v2 -t $HOME/bin bin
+stow -v2 -t $HOME misc

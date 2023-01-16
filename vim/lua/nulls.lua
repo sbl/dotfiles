b3566b05ba23fbaf8419f0a46cbadc996aef27d0
Stephen Lumenta <stephen.lumenta@gmail.com>
2023-01-16 21:38:18 +0100

    isort

--- vim/lua/nulls.lua
+++ vim/lua/nulls.lua
@@ -4,6 +4,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.clang_format,
     null_ls.builtins.formatting.gofmt,
     null_ls.builtins.formatting.goimports,

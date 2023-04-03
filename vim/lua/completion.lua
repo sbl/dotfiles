3d4208a21c5aa8cee4364e7480bb7c7b87ca4c43
Stephen Lumenta <stephen.lumenta@gmail.com>
2023-04-03 23:41:48 +0200

    brute force well behaved cmp

--- vim/lua/completion.lua
+++ vim/lua/completion.lua
@@ -13,6 +13,13 @@ if not cmp then
 end
 
 local luasnip = require('luasnip')
+luasnip.config.set_config({
+  region_check_events = 'InsertEnter',
+  delete_check_events = 'InsertLeave',
+  history = false,
+  updateevents = "TextChanged,TextChangedI"
+})
+
 require("luasnip.loaders.from_vscode").lazy_load({
   paths = {
     vim.fn.stdpath('config') .. '/snippets',
@@ -29,17 +36,13 @@ cmp.setup({
       luasnip.lsp_expand(args.body)
     end,
   },
-
   mapping = {
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs( -4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-c>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-    ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
+    ['<C-c>'] = cmp.config.disable,
+    ['<C-y>'] = cmp.config.disable,
+    ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
@@ -59,19 +62,17 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      elseif luasnip.jumpable( -1) then
+        luasnip.jump( -1)
       else
         fallback()
       end
     end, { "i", "s" }),
 
   },
-
   completion = {
     autocomplete = false,
   },
-
   sources = cmp.config.sources {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },

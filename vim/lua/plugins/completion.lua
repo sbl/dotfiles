8f639e2ae601eb3f3d77279af168053fb6568c06
Stephen Lumenta <stephen.lumenta@gmail.com>
2024-06-23 10:03:43 +0200

    simplify cmp?

--- vim/lua/plugins/completion.lua
+++ vim/lua/plugins/completion.lua
@@ -1,47 +1,25 @@
 vim.o.completeopt = 'menuone,noselect'
 
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 -- Setup nvim-cmp.
-local luasnip = require("luasnip")
 local cmp = require('cmp')
 
+require("luasnip.loaders.from_vscode").load {
+  exclude = "go",
+}
 
 --- CMP config
 
 cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4)),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete()),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      elseif vim.bo.buftype ~= 'prompt' and has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      elseif vim.bo.buftype ~= 'prompt' and has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
   }),
   completion = {
     autocomplete = false,
@@ -54,6 +32,6 @@ cmp.setup({
   sources = cmp.config.sources {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    -- { name = 'buffer' },
+    { name = 'buffer' },
   }
 })

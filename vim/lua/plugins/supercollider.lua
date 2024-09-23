a861f400df7d0ff850e041c44f5ceff38fdf5a04
Stephen Lumenta <stephen.lumenta@gmail.com>
2024-09-23 21:01:29 +0200

    supercollider

---
+++ vim/lua/plugins/supercollider.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"davidgranstrom/scnvim",
+		ft = "supercollider",
+		config = function()
+			local scnvim = require("scnvim")
+			local map = scnvim.map
+
+			scnvim.setup({
+				keymaps = {
+					["<F5>"] = {
+						map("editor.send_block", { "i", "n" }),
+						map("editor.send_selection", "x"),
+					},
+					["<F6>"] = map("editor.send_line", { "i", "n" }),
+					["<leader>sc"] = map("postwin.clear", { "n", "i" }),
+					["<leader>sk"] = map("sclang.recompile", { "n", "i" }),
+					["<leader>ss"] = map("sclang.start", { "n", "i" }),
+					["<C-k>"] = map("signature.show", { "n", "i" }),
+					["<F12>"] = map("sclang.hard_stop", { "n", "x", "i" }),
+				},
+			})
+		end,
+	},
+}

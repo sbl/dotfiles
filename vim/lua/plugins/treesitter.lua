6bce17262c76bb4780d9d483a40add363fd75c7e
Stephen Lumenta <stephen.lumenta@gmail.com>
2026-04-19 14:59:51 +0200

    fix TS install

--- vim/lua/plugins/treesitter.lua
+++ vim/lua/plugins/treesitter.lua
@@ -3,73 +3,71 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			configs.setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"css",
-					"cmake",
-					"cpp",
-					"eex",
-					"elixir",
-					"fish",
-					"go",
-					"gomod",
-					"gosum",
-					"gotmpl",
-					"heex",
-					"html",
-					"htmldjango",
-					"javascript",
-					"lua",
-					"make",
-					"objc",
-					"python",
-					"ron",
-					"ruby",
-					"rust",
-					"scss",
-					"supercollider",
-					"svelte",
-					"swift",
-					"typescript",
-					"vim",
-					"vimdoc",
-					"zig",
-				},
-				highlight = {
+		opts_extend = { "ensure_installed" },
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"cmake",
+				"cpp",
+				"eex",
+				"elixir",
+				"fish",
+				"go",
+				"gomod",
+				"gosum",
+				"gotmpl",
+				"heex",
+				"html",
+				"htmldjango",
+				"javascript",
+				"lua",
+				"make",
+				"objc",
+				"python",
+				"ron",
+				"ruby",
+				"rust",
+				"scss",
+				"supercollider",
+				"svelte",
+				"swift",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"zig",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = true,
+				disable = { "html", "javascript", "typescript" },
+			},
+			indent = {
+				enable = true,
+				disable = { "javascript", "typescript" },
+			},
+			endwise = {
+				enable = true,
+			},
+			matchup = {
+				enable = true,
+			},
+			textobjects = {
+				select = {
 					enable = true,
-					additional_vim_regex_highlighting = true,
-					disable = { "html", "javascript", "typescript" },
-				},
-				indent = {
-					enable = true,
-					disable = { "javascript", "typescript" },
-				},
-				endwise = {
-					enable = true,
-				},
-				matchup = {
-					enable = true,
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
 					},
 				},
-			})
-		end,
+			},
+		},
 		dependencies = {
 			{ "nvim-treesitter/nvim-treesitter-textobjects" },
 			{ "RRethy/nvim-treesitter-endwise" },
